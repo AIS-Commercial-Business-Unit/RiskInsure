bfbcf1554888a51de1a733744f0e1b29d38c8bc0
Steven Suing <steven.suing@ais.com>
2026-02-03 17:34:33 -0500

    Fixed back end state resource group for terraform

--- platform/infra/versions.tf
+++ platform/infra/versions.tf
@@ -15,7 +15,7 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
+    resource_group_name  = "CAIS-010-RiskInsure"
     storage_account_name = "riskinsuretfstate"
     container_name       = "tfstate"
     key                  = "riskinsure.tfstate"
