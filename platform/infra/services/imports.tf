439cf07ae9a0b84006531c6b7da6efb0da4e973b
Meghan Kulkarni <meghan.kulkarni@outlook.com>
2026-02-14 22:40:28 -0500

    feat: add imports for Azure container apps and endpoints

---
+++ platform/infra/services/imports.tf
@@ -0,0 +1,51 @@
+import {
+  to = azurerm_container_app.billing_api
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/billing-api"
+}
+
+import {
+  to = azurerm_container_app.billing_endpoint
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/billing-endpoint"
+}
+
+import {
+  to = azurerm_container_app.customer_api
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/customer-api"
+}
+
+import {
+  to = azurerm_container_app.customer_endpoint
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/customer-endpoint"
+}
+
+import {
+  to = azurerm_container_app.fundstransfermgt_api
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/fundstransfermgt-api"
+}
+
+import {
+  to = azurerm_container_app.fundstransfermgt_endpoint
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/fundstransfermgt-endpoint"
+}
+
+import {
+  to = azurerm_container_app.policy_api
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/policy-api"
+}
+
+import {
+  to = azurerm_container_app.policy_endpoint
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/policy-endpoint"
+}
+
+import {
+  to = azurerm_container_app.ratingandunderwriting_api
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/ratingandunderwriting-api"
+}
+
+import {
+  to = azurerm_container_app.ratingandunderwriting_endpoint
+  id = "/subscriptions/c4fb1c99-fb99-4dc1-9926-a3a4356fd44a/resourceGroups/CAIS-010-RiskInsure/providers/Microsoft.App/containerApps/ratingandunderwriting-endpoint"
+}
+
+
