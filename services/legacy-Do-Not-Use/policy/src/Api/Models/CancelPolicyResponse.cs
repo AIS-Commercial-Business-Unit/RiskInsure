422b69947b2b259a42a1367772105ee887bd6e98
Steven Suing <steven.suing@ais.com>
2026-04-08 15:01:17 -0400

    Removed old code

--- services/legacy-Do-Not-Use/policy/src/Api/Models/CancelPolicyResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace RiskInsure.Policy.Api.Models;
-
-public class CancelPolicyResponse
-{
-    public required string PolicyId { get; set; }
-    public required string Status { get; set; }
-    public DateTimeOffset CancellationDate { get; set; }
-    public decimal UnearnedPremium { get; set; }
-}
--- services/legacy-Do-Not-Use/ratingandunderwriting/src/Domain/Contracts/Events/QuoteDeclined.cs
+++
@@ -1,9 +0,0 @@
-namespace RiskInsure.RatingAndUnderwriting.Domain.Contracts.Events;
-
-public record QuoteDeclined(
-    Guid MessageId,
-    DateTimeOffset OccurredUtc,
-    string QuoteId,
-    string DeclineReason,
-    string IdempotencyKey
-);
